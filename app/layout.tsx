005cab379be20237daade7f77271fbd1f9452cca
yash-shewalkar <iamyashshewalkar@gmail.com>
2025-01-29 22:59:03 +0530

    resume

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -39,6 +40,7 @@ export default function RootLayout({
           >
             <SessionProvider>
             {children}
+            <Analytics />
             </SessionProvider>
             
           </ThemeProvider>
